bd507e3609f3fb05b8fd88b18e523f9610711c42
mzaitsev27 <47281498+mzaitsev27@users.noreply.github.com>
2019-11-23 21:17:57 +0300

    Update DivTestDataProvider.java

--- src/test/java/DataProviders/DivTestDataProvider.java
+++ src/test/java/DataProviders/DivTestDataProvider.java
@@ -5,7 +5,7 @@ import org.testng.annotations.DataProvider;
 public class DivTestDataProvider {
 
     @DataProvider(name = "Div Data Provider")
-    public Object[][] DivTestDataProvider() {
+    public Object[][] divTestDataProvider() {
         return new Object[][]{
                 {2, 2, 1},
                 {10, 5, 2},
