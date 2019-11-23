339ba2d848ad2f5760b23b923c0d15981fa140da
mzaitsev27 <47281498+mzaitsev27@users.noreply.github.com>
2019-11-23 21:18:28 +0300

    Update BaseTest.java

--- src/test/java/Tests/BaseTest.java
+++ src/test/java/Tests/BaseTest.java
@@ -4,7 +4,7 @@ import com.epam.tat.module4.Calculator;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
-public class BaseTest {
+public abstract class BaseTest {
 
     protected Calculator calculator;
 
