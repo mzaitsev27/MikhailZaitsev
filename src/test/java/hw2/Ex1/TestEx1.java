83bd527abcf9d2b679fb02952ae5230f40ae195d
mzaitsev27 <mzaitsev27@gmail.com>
2019-11-26 18:18:39 +0300

    hw2

---
+++ src/test/java/hw2/Ex1/TestEx1.java
@@ -0,0 +1,105 @@
+package hw2.Ex1;
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import org.testng.annotations.Test;
+
+import java.util.*;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+public class TestEx1 {
+
+
+    @Test
+    public void ex1() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+
+        driver.get("https://epam.github.io/JDI/index.html");
+
+        // Assert Browser title
+        assertEquals(driver.getTitle().trim(), "Home Page");
+        // -----------------------------------------------------------
+
+        // Assert User name expected "PITER CHAILOVSKII
+        driver.findElement(By.id("user-icon")).click();
+        driver.findElement(By.cssSelector("#name")).sendKeys("epam");
+        driver.findElement(By.xpath("//input[@id='password']")).sendKeys("1234");
+        driver.findElement(By.id("login-button")).click();
+        WebElement loginedUserName = driver.findElement(By.id("user-name"));
+        assertEquals(loginedUserName.getText().trim(), "PITER CHAILOVSKII");
+        // -----------------------------------------------------------
+
+        // Assert 4 items on the header section "HOME", "CONTACT FORM", "SERVICE", "METALS & COLORS"
+        List<WebElement> elements = driver.findElements(By.tagName("li"));
+        List<String> foundedElements = new ArrayList<>();
+        List<String> expectedElements = new ArrayList<>(Arrays.asList("HOME", "CONTACT FORM", "SERVICE", "METALS & COLORS"));
+        for (int i = 0; i < elements.size(); i++) {
+            WebElement e = elements.get(i);
+            if (e.getText().equals("HOME") || e.getText().equals("CONTACT FORM") || e.getText().equals("SERVICE") || e.getText().equals("METALS & COLORS")) {
+                foundedElements.add(e.getText());
+            }
+        }
+        assertEquals(foundedElements, expectedElements);
+        // --------------------------------------------------------------------------
+
+
+        // Assert 4 images and proper text below each
+        ExpectedConstant ec = null;
+        List<WebElement> images = driver.findElements(By.className("benefit-icon"));
+        List<WebElement> textUnderImages = driver.findElements(By.className("benefit-txt"));
+        List<String> textUnderImagesString = new ArrayList<>();
+        for (int i = 0; i < textUnderImages.size(); i++) {
+            textUnderImagesString.add(textUnderImages.get(i).getText());
+        }
+        List<String> expected = new ArrayList<>(Arrays.asList(ec.textUnderFirstImage, ec.textUnderSecondImage, ec.textUnderThirdImage, ec.textUnderFourthImage));
+        assertEquals(images.size(), 4);
+        assertEquals(textUnderImagesString, expected);
+        // -----------------------------------------------------------------------------------------------
+
+        // Assert a text of the main headers
+        String mainHeader = driver.findElement(By.name("main-title")).getText();
+        String mainHeaderText = driver.findElement(By.name("jdi-text")).getText();
+        assertEquals(mainHeader, ec.mainHeader);
+        assertEquals(mainHeaderText, ec.mainHeaderText);
+        // ---------------------------------------------
+
+
+        // Assert iframe
+        WebElement frame = driver.findElement(By.id("iframe"));
+        assertTrue(frame.isDisplayed());
+        driver.switchTo().frame("iframe");
+        assertTrue(driver.findElement(By.xpath(".//*[@id = 'epam_logo']")).isDisplayed()) ;
+        // --------------------------------------------------------
+
+        // Assert subheader
+        List<WebElement> subHeader = driver.findElements(By.tagName("a"));
+        for (int i = 0; i < subHeader.size(); i++) {
+            if (subHeader.get(i).getText().equals("JDI GITHUB")) {
+                assertEquals(subHeader.get(i).getText(), "JDI GITHUB");
+                assertEquals(subHeader.get(i).getAttribute("href"), "https://github.com/epam/JDI");
+            }
+        }
+        // --------------------------------------------------------------------------
+
+        // Assert left section
+        WebElement section = driver.findElement(By.name("navigation-sidebar"));
+        assertTrue(section.isDisplayed());
+        // ----------------------------------------------------------------------
+
+        // Assert footer
+        WebElement footer = driver.findElement(By.tagName("footer"));
+        assertTrue(footer.isDisplayed());
+        // ----------------------------------------------------
+
+        driver.close();
+    }
+}
+
---
+++ src/test/java/hw2/Ex2/TestEx2.java
@@ -0,0 +1,202 @@
+package hw2.Ex2;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import java.util.List;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+public class TestEx2 {
+    @Test
+    public void ex2() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("https://epam.github.io/JDI/index.html");
+        SoftAssert softAssert = new SoftAssert();
+
+        // Assert Browser title
+        assertEquals(driver.getTitle().trim(), "Home Page");
+        // -----------------------------------------------------------
+
+        // Assert User name expected "PITER CHAILOVSKII
+        driver.findElement(By.id("user-icon")).click();
+        driver.findElement(By.cssSelector("#name")).sendKeys("epam");
+        driver.findElement(By.xpath("//input[@id='password']")).sendKeys("1234");
+        driver.findElement(By.id("login-button")).click();
+        WebElement loginedUserName = driver.findElement(By.id("user-name"));
+        assertEquals(loginedUserName.getText().trim(), "PITER CHAILOVSKII");
+        // -----------------------------------------------------------
+
+
+        // Assert "Service" subcategory in the header
+        WebElement serviceFromHeader = driver.findElement(By.className("dropdown-toggle"));
+        serviceFromHeader.click();
+        softAssert.assertTrue(serviceFromHeader.isDisplayed());
+        // ----------------------------------------------------------------
+
+
+        // Assert "Service" subcategory in the left section
+        WebElement serviceFromLeftSection = driver.findElement(By.className("menu-title"));
+        serviceFromLeftSection.click();
+        softAssert.assertTrue(serviceFromLeftSection.isDisplayed());
+        // -----------------------------------------------------------------
+
+
+        // Open Service - > Different Elements Page
+        driver.findElement(By.className("dropdown-toggle")).click();
+        List<WebElement> header = driver.findElements(By.tagName("li"));
+        for (int i = 0; i < header.size(); i++) {
+            if (header.get(i).getText().equals("DIFFERENT ELEMENTS")) {
+                header.get(i).click();
+                break;
+            }
+        }
+
+        // Assert 4 checkboxes at "Different Eelements" page
+        List<WebElement> checkboxes = driver.findElements(By.xpath("//label[@class = 'label-checkbox']"));
+        assertEquals(checkboxes.size(), 4);
+        for (int i = 0; i < checkboxes.size(); i++) {
+            assertTrue(checkboxes.get(i).isDisplayed());
+        }
+
+
+        // 4 radios
+        List<WebElement> radios = driver.findElements(By.className("label-radio"));
+        assertEquals(radios.size(), 4);
+        for (int i = 0; i < checkboxes.size(); i++) {
+            assertTrue(radios.get(i).isDisplayed());
+        }
+
+        // dropdown
+        WebElement dropdown = driver.findElement(By.tagName("select"));
+        assertTrue(dropdown.isDisplayed());
+
+        // 2 buttons
+        List<WebElement> buttons = driver.findElements(By.className("uui-button"));
+        for (int i = 0; i < buttons.size(); i++) {
+            if (buttons.get(i).getAttribute("value").equals("Default Button") || buttons.get(i).getAttribute("value").equals("Button")) {
+                assertTrue(buttons.get(i).isDisplayed());
+            }
+        }
+        // --------------------------------------------------------------------------------
+
+
+        // Assert that there is Right Section
+        WebElement rightSideBar = driver.findElement(By.id("mCSB_2"));
+        softAssert.assertTrue(rightSideBar.isDisplayed());
+
+        // Assert that there is Left Section
+        WebElement leftSideBar = driver.findElement(By.id("mCSB_1"));
+        softAssert.assertTrue(leftSideBar.isDisplayed());
+        // ----------------------------------------------------------------------
+
+
+        // Select Water and Wind checkboxes
+        for (int i = 0; i < checkboxes.size(); i++) {
+            if (checkboxes.get(i).getText().equals("Water") || checkboxes.get(i).getText().equals("Wind")) {
+                checkboxes.get(i).click();
+            }
+        }
+
+        // -----------------------------------------------------------------------------------------------------
+
+        List<WebElement> logs = driver.findElements(By.xpath(".//li[contains(text(),\"changed\")]"));
+
+        // Assert log for checkboxes
+        List<WebElement> checkboxesInput = driver.findElements(By.xpath("//label[@class = 'label-checkbox']/input"));
+        for (int i = 0; i < checkboxes.size(); i++) {
+            for (int j = 0; j < logs.size(); j++) {
+                if (logs.get(j).getText().contains(checkboxes.get(i).getText())) {
+                    if (logs.get(j).getText().endsWith("true")) {
+                        softAssert.assertEquals(checkboxesInput.get(i).isSelected(), true);
+                    } else {
+                        softAssert.assertEquals(checkboxesInput.get(i).isSelected(), false);
+                        break;
+                    }
+                }
+            }
+        }
+        // -----------------------------------------------------------------------------------
+
+
+        // Select radio "Selen"
+        for (int i = 0; i < radios.size(); i++) {
+            if (radios.get(i).getText().equals("Selen")) {
+                radios.get(i).click();
+                WebElement newLog = driver.findElement(By.xpath(".//li[contains(text(),\"changed\")][position() =1]"));
+                logs.add(newLog);
+            }
+        }
+
+
+        // Assert log for radios
+        List<WebElement> radioInput = driver.findElements(By.xpath("//label[@class = 'label-radio']/input"));
+        for (int i = 0; i < radios.size(); i++) {
+            for (int j = logs.size() - 1; j > 0; j--) {
+                if (logs.get(j).getText().contains(radios.get(i).getText())) {
+                    softAssert.assertEquals(radioInput.get(i).isSelected(), true);
+                } else {
+                    softAssert.assertEquals(radioInput.get(i).isSelected(), false);
+                }
+                break;
+            }
+        }
+
+        // Select "Yellow" in dropdown
+        dropdown.click();
+        List<WebElement> colors = driver.findElements(By.tagName("option"));
+        for (int i = 0; i < colors.size(); i++) {
+            if (colors.get(i).getText().equals("Yellow")) {
+                colors.get(i).click();
+                WebElement newLog = driver.findElement(By.xpath(".//li[contains(text(),\"changed\")][position() =1]"));
+                logs.add(newLog);
+            }
+        }
+
+        // Assert log for color
+        for (int i = 0; i < colors.size(); i++) {
+            for (int j = logs.size() - 1; j > 0; j--) {
+                if (logs.get(j).getText().contains(colors.get(i).getText())) {
+                    softAssert.assertEquals(colors.get(i).getText(), "Yellow");
+                }
+                break;
+            }
+        }
+
+        // Unselect checkboxes "Water" and "Wind"
+        for (int i = 0; i < checkboxes.size(); i++) {
+            if (checkboxes.get(i).getText().equals("Water") || checkboxes.get(i).getText().equals("Wind")) {
+                checkboxes.get(i).click();
+                WebElement newLog = driver.findElement(By.xpath(".//li[contains(text(),\"changed\")][position() =1]"));
+                logs.add(newLog);
+            }
+        }
+
+        // Assert that checkboxes "Water" and "Wind" unselected
+        for (int i = 0; i < checkboxes.size(); i++) {
+            for (int j = logs.size() - 1; j > 0; j--) {
+                if (logs.get(j).getText().contains(checkboxes.get(i).getText())) {
+                    if (logs.get(j).getText().endsWith("true")) {
+                        softAssert.assertEquals(checkboxesInput.get(i).isSelected(), true);
+                    } else {
+                        softAssert.assertEquals(checkboxesInput.get(i).isSelected(), false);
+                        break;
+                    }
+                }
+            }
+        }
+
+        softAssert.assertAll();
+        driver.close();
+    }
+}
+
