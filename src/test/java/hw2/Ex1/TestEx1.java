9ee45f6b02dcf311c2625d45d9f8761bdf98d64f
mzaitsev27 <47281498+mzaitsev27@users.noreply.github.com>
2019-11-27 23:42:13 +0300

    Update TestEx1.java

--- src/test/java/hw2/Ex1/TestEx1.java
+++ src/test/java/hw2/Ex1/TestEx1.java
@@ -18,10 +18,6 @@ public class TestEx1 {
 
     @Test
     public void ex1() {
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-
 
         driver.get("https://epam.github.io/JDI/index.html");
 
@@ -30,23 +26,17 @@ public class TestEx1 {
         // -----------------------------------------------------------
 
         // Assert User name expected "PITER CHAILOVSKII
-        driver.findElement(By.id("user-icon")).click();
-        driver.findElement(By.cssSelector("#name")).sendKeys("epam");
-        driver.findElement(By.xpath("//input[@id='password']")).sendKeys("1234");
-        driver.findElement(By.id("login-button")).click();
+        login();
         WebElement loginedUserName = driver.findElement(By.id("user-name"));
         assertEquals(loginedUserName.getText().trim(), "PITER CHAILOVSKII");
         // -----------------------------------------------------------
 
         // Assert 4 items on the header section "HOME", "CONTACT FORM", "SERVICE", "METALS & COLORS"
-        List<WebElement> elements = driver.findElements(By.tagName("li"));
+        List<WebElement> elements = driver.findElements(By.xpath("//ul[@class = 'uui-navigation nav navbar-nav m-l8']/li/a"));
         List<String> foundedElements = new ArrayList<>();
         List<String> expectedElements = new ArrayList<>(Arrays.asList("HOME", "CONTACT FORM", "SERVICE", "METALS & COLORS"));
         for (int i = 0; i < elements.size(); i++) {
-            WebElement e = elements.get(i);
-            if (e.getText().equals("HOME") || e.getText().equals("CONTACT FORM") || e.getText().equals("SERVICE") || e.getText().equals("METALS & COLORS")) {
-                foundedElements.add(e.getText());
-            }
+            foundedElements.add(elements.get(i).getText());
         }
         assertEquals(foundedElements, expectedElements);
         // --------------------------------------------------------------------------
@@ -60,7 +50,7 @@ public class TestEx1 {
         for (int i = 0; i < textUnderImages.size(); i++) {
             textUnderImagesString.add(textUnderImages.get(i).getText());
         }
-        List<String> expected = new ArrayList<>(Arrays.asList(ec.textUnderFirstImage, ec.textUnderSecondImage, ec.textUnderThirdImage, ec.textUnderFourthImage));
+        List<String> expected = new ArrayList<>(Arrays.asList(ec.firstImageText, ec.secondImageText, ec.thirdImageText, ec.fourthImageText));
         assertEquals(images.size(), 4);
         assertEquals(textUnderImagesString, expected);
         // -----------------------------------------------------------------------------------------------
@@ -77,17 +67,14 @@ public class TestEx1 {
         WebElement frame = driver.findElement(By.id("iframe"));
         assertTrue(frame.isDisplayed());
         driver.switchTo().frame("iframe");
-        assertTrue(driver.findElement(By.xpath(".//*[@id = 'epam_logo']")).isDisplayed()) ;
+        assertTrue(driver.findElement(By.xpath("//div[@class='epam-logo']//a//img")).isDisplayed());
+        driver.switchTo().defaultContent();
         // --------------------------------------------------------
 
         // Assert subheader
-        List<WebElement> subHeader = driver.findElements(By.tagName("a"));
-        for (int i = 0; i < subHeader.size(); i++) {
-            if (subHeader.get(i).getText().equals("JDI GITHUB")) {
-                assertEquals(subHeader.get(i).getText(), "JDI GITHUB");
-                assertEquals(subHeader.get(i).getAttribute("href"), "https://github.com/epam/JDI");
-            }
-        }
+        WebElement subHeader = driver.findElement(By.xpath("//a[@href = 'https://github.com/epam/JDI']"));
+        assertEquals(subHeader.getText(), "JDI GITHUB");
+        assertEquals(subHeader.getAttribute("href"), "https://github.com/epam/JDI");
         // --------------------------------------------------------------------------
 
         // Assert left section
@@ -99,8 +86,6 @@ public class TestEx1 {
         WebElement footer = driver.findElement(By.tagName("footer"));
         assertTrue(footer.isDisplayed());
         // ----------------------------------------------------
-
-        driver.close();
     }
 }
 
